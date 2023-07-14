1634cd804381bbb821c5c838a1d2bae27fe3223e
GXS-FIN <smithgranville@gmail.com>
2023-07-14 06:32:13 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,21 +26,18 @@ else
 {
 lastDigit = n % 10;
 }
-
-printf("Last digit of %d is ", n);
-
+printf("Last digit of %d is %d", n, lastDigit);
 if (lastDigit > 5)
 {
-printf("greater than 5\n");
+printf(" and is greater than 5\n");
 }
 else if (lastDigit == 0)
 {
-printf("0\n");
+printf(" and is 0\n");
 }
 else
 {
-printf("less than 6 and not 0\n");
+printf(" and is less than 6 and not 0\n");
 }
-
 return (0);
 }
