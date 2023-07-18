cf0548f22b4077ca07a16bec281f782f34bbb5dc
GXS-FIN <smithgranville@gmail.com>
2023-07-18 08:12:38 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,7 +15,7 @@ for (j = 0; j < 26; j++)
 _putchar(letter);
 letter++;
 }
-letter = 'a'; // Reset letter to 'a' for the next line
+letter = 'a'; /* Reset letter to 'a' for the next line */
 _putchar('\n');
 }
 }
